8ef45c0ab3ee2acbf0067c0cf9da4b0a71b631b4
Sean Lockwood <lockwood@stsci.edu>
2015-05-28 13:45:03 -0400

    Added nose tests for superdark_hash().

--- stis_cti/unit_tests.py
+++ stis_cti/unit_tests.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-from nose.tools import assert_equals, assert_false, assert_raises
+from nose.tools import assert_equals, assert_false, assert_raises, assert_not_equals
 import os
 import datetime
 from astropy.io import fits
 
-#from stis_cti import resolve_iraf_file, viable_ccd_file, superdark_hash
 from stis_cti import *
 from archive_dark_query import archive_dark_query
 
@@ -177,7 +176,7 @@ def write_pctetab(pctetab):
     hdu.header['NCHGLEAK'] = (1, 'number of chg_leak extensions')
     hdu.header['INSTRUME'] = 'STIS'                                                            
     hdu.header['DETECTOR'] = 'CCD'                                                            
-    hdu.header['SIM_NIT '] = (7, 'number of readout simulations done per column')
+    hdu.header['SIM_NIT']  = (7, 'number of readout simulations done per column')
     hdu.header['SHFT_NIT'] = (4, 'the number of shifts each column readout simula')
     hdu.header['RN_CLIP']  = (5.6, 'Read noise level in electrons.')
     hdu.header['NSEMODEL'] = (1, 'Read noise smoothing algorithm.')
@@ -208,6 +207,155 @@ class Test_check_pctetab_version:
         fits.setval(self.pctetab, 'PCTE_VER', value='3.0_beta')
         assert_raises(VersionError, check_pctetab_version, self.pctetab, False, '0.1', '1.999')
 
+
+def write_superdark(superdark):
+    if not os.access(os.path.curdir, os.W_OK):
+        raise Exception('Can\'t write test superdark file to CWD!')
+    
+    hdu = fits.PrimaryHDU()
+    
+    hdu.header['FILENAME'] = superdark
+    hdu.header['FILETYPE'] = 'DARK IMAGE'
+    hdu.header['TELESCOP'] = 'HST'
+    hdu.header['REF_TEMP'] = 18
+    hdu.header['DRK_VS_T'] = 0.07
+    hdu.header['PCTECORR'] = 'COMPLETE'
+    hdu.header['PCTETAB']  = 'pctetab$a01_stis_pcte.fits'
+    hdu.header['USEAFTER'] = 'Jul 24 2012 00:59:27'
+    hdu.header['PEDIGREE'] = 'INFLIGHT 01/10/1996 25/06/2012'
+    hdu.header['DESCRIP']  = 'Weekly gain=1 dark for STIS CCD data taken after Jul 24 2012-------'
+    hdu.header['INSTRUME'] = 'STIS'
+    hdu.header['DETECTOR'] = ('CCD', 'detector in use: CCD')
+    
+    hdu.header['PCTEFRAC'] = (1.192887185841836,              'CTE time scaling value')
+    hdu.header['PCTESMIT'] = (7,                              'PCTE readout simulation iterations')
+    hdu.header['PCTESHFT'] = (4,                              'PCTE readout number of shifts')
+    hdu.header['PCTERNCL'] = (5.6,                            'PCTE readnoise amplitude')
+    hdu.header['PCTENSMD'] = (1,                              'PCTE readnoise mitigation algorithm')
+    hdu.header['PCTETRSH'] = (-30.0,                          'PCTE over-subtraction threshold')
+    hdu.header['PCTE_VER'] = ('0.1_alpha',                    'Version of PCTETAB')
+    hdu.header['CTE_NAME'] = ('PixelCTE 2012',                'name of CTE algorithm')
+    hdu.header['CTE_VER']  = ('3.2',                          'version of CTE algorithm')
+    hdu.header['BASEDARK'] = ('$ctitest/bdark_d133_drk.fits', 'Used to make weekdark')
+    
+    hdu.header['HISTORY'] = 'blah, blah blah'
+    hdu.header['HISTORY'] = 'The following input files were used:'
+    hdu.header['HISTORY'] = 'abc_cte.fits'
+    hdu.header['HISTORY'] = 'def_cte.fits'
+    hdu.header['HISTORY'] = 'hij_cte.fits'
+    hdu.header['HISTORY'] = ''
+    hdu.header['HISTORY'] = 'blah2, blah2, blah2'
+    
+    hdu.writeto(superdark, output_verify='exception', clobber=True)
+
+
+class Test_superdark_hash:
+    '''
+    Tests functionality of stis_cti.superdark_hash
+    '''
+    pctetab = 'a00_stis_000test37166_pcte.fits'
+    superdark = 'd001_testfile57254_drk.fits'
+    
+    def setup(self):
+        write_superdark(self.superdark)
+        write_pctetab(self.pctetab)
+    
+    def teardown(self):
+        os.remove(self.superdark)
+        os.remove(self.pctetab)
+    
+    def test_manual_inputs_pass(self):
+        assert_equals(
+            superdark_hash(sim_nit=7, shft_nit=4, rn_clip=5.6, nsemodel=1, subthrsh=-30.0, 
+                           pcte_ver='0.1_alpha', files=[]), 
+            superdark_hash(pctetab=self.pctetab, files=[]))
+    
+    def test_manual_inputs2_pass(self):
+       # superdark_hash should ignore text after '_' in pcte_ver
+        assert_equals(
+            superdark_hash(sim_nit=7, shft_nit=4, rn_clip=5.6, nsemodel=1, subthrsh=-30.0, 
+                           pcte_ver='0.1_beta', files=[]), 
+            superdark_hash(pctetab=self.pctetab, files=[]))
+    
+    def test_manual_inputs3_pass(self):
+        # try with a file list specified
+        assert_equals(
+            superdark_hash(sim_nit=7, shft_nit=4, rn_clip=5.6, nsemodel=1, subthrsh=-30.0, 
+                           pcte_ver='0.1_alpha', files=['abc_cte.fits', 'def_cte.fits', 'hij_cte.fits']), 
+            superdark_hash(pctetab=self.pctetab, files=['abc_cte.fits', 'def_cte.fits', 'hij_cte.fits']))
+    
+    def test_manual_inputs4_pass(self):
+        assert_equals(
+            superdark_hash(sim_nit=7, shft_nit=4, rn_clip=5.6, nsemodel=1, subthrsh=-30.0, 
+                           pcte_ver='0.1_alpha', files=['abc_cte.fits', 'def_cte.fits', 'hij_cte.fits']), 
+            superdark_hash(superdark=self.superdark))
+    
+    def test_manual_inputs_reject(self):
+        # changed file list
+        assert_not_equals(
+            superdark_hash(sim_nit=7, shft_nit=4, rn_clip=5.6, nsemodel=1, subthrsh=-30.0, 
+                           pcte_ver='0.1_alpha', files=[]), 
+            superdark_hash(pctetab=self.pctetab, files=['otherfile_cte.fits']))
+    
+    def test_manual_inputs2_reject(self):
+        # changed sim_nit
+        assert_not_equals(
+            superdark_hash(sim_nit=70, shft_nit=4, rn_clip=5.6, nsemodel=1, subthrsh=-30.0, 
+                           pcte_ver='0.1_alpha', files=[]), 
+            superdark_hash(pctetab=self.pctetab, files=[]))
+    
+    def test_manual_inputs3_reject(self):
+        # changed pcte_ver
+        assert_not_equals(
+            superdark_hash(sim_nit=7, shft_nit=4, rn_clip=5.6, nsemodel=1, subthrsh=-30.0, 
+                           pcte_ver='0.2_alpha', files=[]), 
+            superdark_hash(pctetab=self.pctetab, files=[]))
+    
+    def test_superdark_pass(self):
+        assert_equals(
+            superdark_hash(superdark=self.superdark, files=[]), 
+            superdark_hash(pctetab=self.pctetab, files=[]))
+    
+    def test_superdark2_pass(self):
+        assert_equals(
+            superdark_hash(superdark=self.superdark), 
+            superdark_hash(pctetab=self.pctetab, files=['abc_cte.fits', 'def_cte.fits', 'hij_cte.fits']))
+    
+    def test_superdark3_pass(self):
+        fits.setval(self.superdark, 'PCTE_VER', value='0.1_gamma')
+        assert_equals(
+            superdark_hash(superdark=self.superdark), 
+            superdark_hash(pctetab=self.pctetab, files=['abc_cte.fits', 'def_cte.fits', 'hij_cte.fits']))
+    
+    def test_superdark_reject(self):
+        assert_not_equals(
+            superdark_hash(superdark=self.superdark), 
+            superdark_hash(pctetab=self.pctetab, files=[]))
+    
+    def test_superdark2_reject(self):
+        fits.setval(self.superdark, 'PCTENSMD', value=5)
+        assert_not_equals(
+            superdark_hash(superdark=self.superdark), 
+            superdark_hash(pctetab=self.pctetab, files=['abc_cte.fits', 'def_cte.fits', 'hij_cte.fits']))
+    
+    def test_superdark3_reject(self):
+        fits.setval(self.pctetab, 'nsemodel', value=6)
+        assert_not_equals(
+            superdark_hash(superdark=self.superdark), 
+            superdark_hash(pctetab=self.pctetab, files=['abc_cte.fits', 'def_cte.fits', 'hij_cte.fits']))
+    
+    def test_superdark4_reject(self):
+        fits.setval(self.superdark, 'PCTE_VER', value='0.2_alpha')
+        assert_not_equals(
+            superdark_hash(superdark=self.superdark), 
+            superdark_hash(pctetab=self.pctetab, files=['abc_cte.fits', 'def_cte.fits', 'hij_cte.fits']))
+    
+    def test_superdark4_reject(self):
+        fits.setval(self.pctetab, 'PCTE_VER', value='0.2_alpha')
+        assert_not_equals(
+            superdark_hash(superdark=self.superdark), 
+            superdark_hash(pctetab=self.pctetab, files=['abc_cte.fits', 'def_cte.fits', 'hij_cte.fits']))
+    
 
 class Test_archive_dark_query:
     '''
