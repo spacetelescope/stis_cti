a3db26567d34f346711d2da54093ac2834e5cae1
Sean Lockwood <sean-lockwood@users.noreply.github.com>
2024-03-01 15:36:23 -0500

    Get modules to build on readthedocs (#20)
    * Added root path in an attempt to fix autodoc on RTD.
    
    * Added RTD package install method of pip.
    
    * mocking stis_cti.StisPixCte_FixY when building docs.

--- doc/conf.py
+++ doc/conf.py
@@ -14,11 +14,14 @@
 
 import sys
 import os
+import mock
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+
+sys.modules['stis_cti.StisPixCte_FixY'] = mock.Mock()
 
 # -- General configuration ------------------------------------------------
 
