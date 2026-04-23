03cd2468b65e7fa858f05e58afa0ff3b3bb227cf
Sean Lockwood <lockwood@stsci.edu>
2026-04-23 18:38:39 -0400

    Reconfigured test location and associated imports.

--- stis_cti/__init__.py
+++ stis_cti/__init__.py
@@ -5,8 +5,11 @@ stis_cti.stis_cti(science_dir, dark_dir, ref_dir, num_processes, pctetab=None,
                   all_weeks_flag=False, allow=False, clean=False, clean_all=False, verbose=1)
 '''
 
-from .stis_cti import stis_cti, __author__
-from .stis_cti import viable_ccd_file
+from .stis_cti import (
+    stis_cti, __author__, viable_ccd_file, determine_input_science, resolve_iraf_file,
+    check_pctetab_version, superdark_hash, check_for_old_output_files,
+    VersionError, FileError
+)
 from .custom_superdark_info import custom_superdark_info
 from . import StisPixCteCorr
 from .archive_dark_query import archive_dark_query
